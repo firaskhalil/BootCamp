84b66fcaa3391c5d761376b22068cc782222e66b
firas khalil <firaskhalilkhalil@gmail.com>
2022-03-23 14:22:32 +0300

    Add project files.

---
+++ Delta/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Delta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("b: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("a: ");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("c: ");
+                int c = Convert.ToInt32(Console.ReadLine());
+
+                int delta = (b * b) - 4 * a * c;
+                Console.WriteLine("Delta: " + delta);
+
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Please just enter  numbers");
+            }
+        }
+    }
+}
